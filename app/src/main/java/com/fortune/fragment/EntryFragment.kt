94446dddff76e7be64707d0fe3177f8cb2486a02
risa.buto <risa.buto@sublimecons.com>
2020-12-07 16:04:26 +0900

    2020/12/07 ランダムにスターアイコンを表示

--- app/src/main/java/com/fortune/fragment/EntryFragment.kt
+++ app/src/main/java/com/fortune/fragment/EntryFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.fortune.R
 
@@ -20,6 +21,10 @@ class EntryFragment : Fragment() {
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         val view = inflater.inflate(R.layout.fragment_entry, container, false)
+        val num = (1..2).random()
+        if (num == 2){
+            view.findViewById<ImageView>(R.id.new_icon).visibility = View.GONE
+        }
 
         // ボタン押下でFortuneResultFragmentに移動
         view?.findViewById<Button>(R.id.button_telling_fortune)?.setOnClickListener {
